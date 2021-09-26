463f15b913a343d20aedba2715523246bc6d2d5b
Justin Moser <jmmoser15@gmail.com>
2021-09-26 11:41:57 -0500

    Allow entire post preview to be clicked

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -2,15 +2,13 @@ import Link from 'next/link';
 
 export default function PostPreview({ post }) {
   return (
-    <div className="overflow-hidden rounded shadow border p-4 hover:text-blue-600 focus:text-blue-600 hover:bg-gray-100">
-      <Link href={`/posts/${post.id}`}>
-        <a>
-          <div className="font-bold mb-2">{post.title}</div>
-          <div className="overflow-ellipsis overflow-hidden whitespace-nowrap">
-            {post.body}
-          </div>
-        </a>
-      </Link>
-    </div>
+    <Link href={`/posts/${post.id}`}>
+      <a className="overflow-hidden rounded shadow border p-4 hover:text-blue-600 focus:text-blue-600 hover:bg-gray-100">
+        <div className="font-bold mb-2">{post.title}</div>
+        <div className="overflow-ellipsis overflow-hidden whitespace-nowrap">
+          {post.body}
+        </div>
+      </a>
+    </Link>
   );
 }
